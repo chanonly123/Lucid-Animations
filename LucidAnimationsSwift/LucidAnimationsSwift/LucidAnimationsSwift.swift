fd5caf67b2b5470ad00d6dd3033f8ad805a71c55
Chandan Karmakar <chan.only.123@gmail.com>
2021-01-25 15:43:50 +0530

    initial commit

---
+++ LucidAnimationsSwift/LucidAnimationsSwift/LucidAnimationsSwift.swift
@@ -0,0 +1,160 @@
+//
+//  LucidAnimationsSwift.swift
+//  LucidAnimationsSwift
+//
+//  Created by Chandan Karmakar on 22/01/21.
+//
+
+import UIKit
+
+public let lucid = LucidAnim()
+
+struct LucidAnimValues {
+    var anim: Any = {} // (()->Void) or ((Int)->Void)
+    
+    var animated: Bool = true
+    
+    //
+    var duration: Double = 0.3
+    
+    //
+    var delay: Double = 0.3
+    
+    //
+    var options: UIView.AnimationOptions = []
+    
+    //
+    var bounce: Bool = false
+    
+    // usingSpringWithDamping
+    var damping: CGFloat = 0.5
+    
+    // initialSpringVelocity
+    var velocity: CGFloat = 0.0
+    
+    // view index for sequential view animations
+    var viewIndex = 0
+    
+    func callAnimClosure() {
+        if let anim = anim as? (() -> Void) {
+            anim()
+        } else if let anim = anim as? ((Int) -> Void) {
+            anim(viewIndex)
+        }
+    }
+    
+    func isSerialAnim() -> Bool {
+        return anim is ((Int) -> Void)
+    }
+}
+
+public class LucidAnim {
+    var queue = [LucidAnimValues]()
+    var currentValues = LucidAnimValues()
+    
+//    func clear() {
+//        queue.removeAll()
+//        currentValues = SimpleAnimValues()
+//    }
+    
+    public func flat(comp: @escaping (() -> Void)) {
+        currentValues.animated = false
+        currentValues.anim = comp
+        
+        queue.append(currentValues)
+        currentValues = LucidAnimValues()
+    }
+    
+    public func anim(anim: @escaping (() -> Void)) {
+        currentValues.animated = true
+        currentValues.anim = anim
+        
+        queue.append(currentValues)
+        currentValues = LucidAnimValues()
+    }
+    
+    public func serially(count: Int, interval: Double, anim: @escaping ((Int) -> Void)) {
+        currentValues.animated = true
+        currentValues.anim = anim
+        
+        for i in 0 ..< count {
+            if i > 0 {
+                currentValues.delay = Double(i) * interval
+            }
+            currentValues.viewIndex = i
+            currentValues.animated = true
+            currentValues.anim = anim
+            queue.append(currentValues)
+        }
+        currentValues = LucidAnimValues()
+    }
+    
+    public func execute() {
+        execute_()
+    }
+
+    func execute_() {
+        if let each = queue.first {
+            queue.removeFirst()
+            if each.animated {
+                let shouldRunSerial = (queue.first?.isSerialAnim() ?? false) && each.isSerialAnim()
+                
+                let animEnd: ((Bool)->Void) = { finished in
+                    if !finished { return }
+                    if !shouldRunSerial {
+                        self.execute_()
+                    }
+                }
+                
+                if each.bounce {
+                    UIView.animate(withDuration: each.duration, delay: each.delay, usingSpringWithDamping: each.damping, initialSpringVelocity: each.velocity, options: each.options, animations: {
+                        each.callAnimClosure()
+                    }, completion: animEnd)
+                } else {
+                    UIView.animate(withDuration: each.duration, delay: each.delay, options: each.options, animations: {
+                        each.callAnimClosure()
+                    }, completion: animEnd)
+                }
+                
+                if shouldRunSerial {
+                    self.execute_()
+                }
+            } else {
+                each.callAnimClosure()
+                self.execute_()
+            }
+        }
+    }
+    
+    // MARK: getter and setters
+
+    public func set(duration: Double) -> Self {
+        currentValues.duration = duration
+        return self
+    }
+    
+    public func set(delay: Double) -> Self {
+        currentValues.delay = delay
+        return self
+    }
+    
+    public func set(options: UIView.AnimationOptions) -> Self {
+        currentValues.options = options
+        return self
+    }
+    
+    public func set(bounce: Bool) -> Self {
+        currentValues.bounce = bounce
+        return self
+    }
+    
+    public func set(damping: CGFloat) -> Self {
+        currentValues.damping = damping
+        return self
+    }
+    
+    public func set(velocity: CGFloat) -> Self {
+        currentValues.velocity = velocity
+        return self
+    }
+}
---
+++ LucidAnimationsSwift/LucidAnimationsSwift/SimpleAnimationsSwift.h
@@ -0,0 +1,18 @@
+//
+//  SimpleAnimationsSwift.h
+//  SimpleAnimationsSwift
+//
+//  Created by Chandan Karmakar on 22/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleAnimationsSwift.
+FOUNDATION_EXPORT double SimpleAnimationsSwiftVersionNumber;
+
+//! Project version string for SimpleAnimationsSwift.
+FOUNDATION_EXPORT const unsigned char SimpleAnimationsSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleAnimationsSwift/PublicHeader.h>
+
+
